915cd851b387e98f23b746c752cf481dec677604
Md Rezaul Karim Shatno <63625974+mdrkshanto@users.noreply.github.com>
2021-09-11 19:23:49 +0600

    Updated

--- resources/views/backEnd/mastering/includes/bottomAssets.blade.php
+++ resources/views/backEnd/mastering/includes/bottomAssets.blade.php
@@ -3,39 +3,10 @@
 <script src="{{ asset('backEnd/assets') }}/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('backEnd/assets') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables  & Plugins -->
-<script src="{{ asset('backEnd/assets') }}/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/jszip/jszip.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('backEnd/assets') }}/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{ asset('backEnd/assets') }}/dist/js/demo.js"></script>
 <!-- Page specific script -->
-<script>
-    $(function () {
-      $("#example1").DataTable({
-        "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["copy", "csv", "excel", "pdf", "print", /*"colvis"*/]
-      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-      $('#example2').DataTable({
-        "paging": true,
-        "lengthChange": false,
-        "searching": false,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false,
-        "responsive": true,
-      });
-    });
-  </script>
+@yield('backEndExtraJs')
 </html>
