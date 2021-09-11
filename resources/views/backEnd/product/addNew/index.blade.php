4137281f5b6bf0b47a51edddd415dbae5c2a4255
Md Rezaul Karim Shatno <63625974+mdrkshanto@users.noreply.github.com>
2021-09-11 16:39:32 +0600

    Update index.blade.php

--- resources/views/backEnd/product/addNew/index.blade.php
+++ resources/views/backEnd/product/addNew/index.blade.php
@@ -18,35 +18,7 @@ Create a new Product
           </div>
           <!-- /.card-header -->
           <div class="card-body">
-            <table id="example1" class="table table-sm table-dark table-bordered table-striped align-middle text-center table-hover small">
-              <thead>
-                  <tr>
-                      <th>Rendering engine</th>
-                      <th>Browser</th>
-                      <th>Platform(s)</th>
-                      <th>Engine version</th>
-                      <th>CSS grade</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td>Trident</td>
-                      <td>Internet Explorer 4.0</td>
-                      <td>Win 95+</td>
-                      <td> 4</td>
-                      <td>X</td>
-                  </tr>
-              </tbody>
-              <tfoot>
-                  <tr>
-                      <th>Rendering engine</th>
-                      <th>Browser</th>
-                      <th>Platform(s)</th>
-                      <th>Engine version</th>
-                      <th>CSS grade</th>
-                  </tr>
-              </tfoot>
-            </table>
+              
           </div>
           <!-- /.card-body -->
         </div>
