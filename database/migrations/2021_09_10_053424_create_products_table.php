f1f48a4d51deb79d3571452ed9938e838cad5d3b
Md Rezaul Karim Shatno <63625974+mdrkshanto@users.noreply.github.com>
2021-09-13 10:25:54 +0600

    Update 2021_09_10_053424_create_products_table.php

--- database/migrations/2021_09_10_053424_create_products_table.php
+++ database/migrations/2021_09_10_053424_create_products_table.php
@@ -15,6 +15,18 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('productName')->required();
+            $table->string('productCetegory')->required();
+            $table->string('productBrand')->required();
+            $table->string('productColor')->nullable();
+            $table->numeric('productRegularPrice')->nullable();
+            $table->numeric('productDiscountPrice')->nullable();
+            $table->text('productDetails')->required();
+            $table->text('productShortDescription')->required();
+            $table->text('productDescription')->required();
+            $table->text('productDeliveryProcess')->required();
+            $table->string('productImages')->nullable();
+            $table->string('productStatus')->required();
             $table->timestamps();
         });
     }
