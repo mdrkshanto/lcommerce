18e9691793c91f6d11e587ea136873027cb9c828
Md Rezaul Karim Shatno <63625974+mdrkshanto@users.noreply.github.com>
2021-09-14 08:18:05 +0600

    Updated

---
+++ resources/views/backEnd/brands/edit/index.blade.php
@@ -0,0 +1,53 @@
+@extends('backEnd.mastering.index')
+@section('backEndTitle')
+Edit a {{ $data->categoryName }} category
+@endsection
+@section('backEndBody')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+          <div class="card-header">
+              <div class="row justify-content-between">
+                  <h3 class="card-title">@yield('backEndTitle')</h3>
+                  <a href="{{ route('categoryList') }}" class="btn btn-sm btn-primary shadow-none fw-bolder">Category list</a>
+              </div>
+          </div>
+          <!-- /.card-header -->
+            <form action="{{ route('updateCategory',[$data->id]) }}" method="post" autocomplete="off">
+                @csrf
+                <div class="card-body">
+                    <div class="container col-6">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="form-label">Category Name</label>
+                                    <input type="text" class="form-control form-control-sm" name="categoryName" placeholder="Category Name" value="{{ $data->categoryName }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="form-label">Category Status</label>
+                                    <select class="form-control form-control-sm text-center" name="categoryStatus">
+                                        <option value="0">Inactive</option>
+                                        <option value="1" selected>Active</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="container col-6">
+                                <button type="submit" class="btn btn-block btn-primary">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+</section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@endsection
---
+++ resources/views/backEnd/brands/list/index.blade.php
@@ -0,0 +1,104 @@
+@extends('backEnd.mastering.index')
+@section('backEndTitle')
+Category List
+@endsection
+@section('backEndBody')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        @if (Session::get('msg'))
+        {{ Session::get('msg') }}
+        @endif
+        <!-- Main content -->
+      <section class="content">
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <div class="row justify-content-between">
+                    <h3 class="card-title">@yield('backEndTitle')</h3>
+                    <a href="{{ route('addCategory') }}" class="btn btn-sm btn-primary shadow-none fw-bolder">Add New</a>
+                </div>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table id="example1" class="table table-sm table-dark table-striped align-middle text-center table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Category Name</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php($i = 1)
+                    @foreach ($categories as $category)
+                    <tr>
+                        <td>{{ $i++ }}</td>
+                        <td>{{ $category->categoryName }}</td>
+                        <td>{{ $category->status == '1' ? 'Active' : 'Inactive' }}</td>
+                        <td>
+                          <div class="input-group input-group-sm btn-group justify-content-center">
+                            <div class="input-group-prepend">
+                              <a href="{{ route('editCategory',[$category->id]) }}" class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></a>
+                            </div>
+                            <div class="input-group-apend">
+                              <form action="{{ route('deleteCategory',[$category->id]) }}" method="post">
+                                @csrf
+                                <button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash-alt"></i></button>
+                              </form>
+                            </div>
+                          </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th>#</th>
+                        <th>Category Name</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+        <!-- /.card -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+@endsection
+@section('backEndExtraJs')
+<!-- DataTables  & Plugins -->
+<script src="{{ asset('backEnd/assets') }}/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/jszip/jszip.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="{{ asset('backEnd/assets') }}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script>
+    $(function () {
+      $("#example1").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", /*"colvis"*/]
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+      });
+    });
+</script>
+@endsection
